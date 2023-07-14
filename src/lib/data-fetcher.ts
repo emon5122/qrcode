6622779e06a66f1c348724b2d60868bb08040b61
emon5122 <emon5122@gmail.com>
2023-07-14 22:48:14 +0600

    axios added

---
+++ src/lib/data-fetcher.ts
@@ -0,0 +1,4 @@
+import axios from "axios";
+
+const url: string =process.env.NODE_ENV ==="production"? "https://abc.xyz":"http://localhost:3000"
+export const myAxios = axios.create({baseURL:`${url}/api/`})
