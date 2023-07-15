19600759922a11cd2bf4796b87103d2c0744ae65
emon5122 <emon5122@gmail.com>
2023-07-15 14:50:19 +0600

    authentication

--- src/app/api/auth/[...nextauth]/authoptions.ts
+++ src/app/api/auth/[...nextauth]/authoptions.ts
@@ -1,2 +1,23 @@
-const AuthOptions ={}
-export default AuthOptions
+import { prisma } from "@/lib/db";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import type { NextAuthOptions } from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+
+const AuthOptions: NextAuthOptions = {
+    adapter:PrismaAdapter(prisma),
+    providers: [
+        GitHubProvider({
+            clientId: process.env.GITHUB_CLIENT_ID as string,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+        }),
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+        }),
+    ],
+    session: {
+        strategy: "jwt",
+    },
+};
+export default AuthOptions;
